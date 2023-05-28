99930715dd226a9ff9893b1172aaa39e5c33603b
JingusJohn <branchjohnchristopher@gmail.com>
2023-05-28 17:20:02 -0500

    improved the table of contents and wrote some more

--- docs/webdocs/src/components/TOC.svelte
+++ docs/webdocs/src/components/TOC.svelte
@@ -1,32 +1,6 @@
 <script lang="ts">
 
-    type tabItem = {
-        name: string,
-        path: string,
-    };
-
-    type tab = {
-        name: string,
-        collapsed: boolean,
-        tabItems: tabItem[],
-    };
-
-    const tabs: tab[] = [
-        {
-            name: "Start Here",
-            collapsed: false,
-            tabItems: [
-                {
-                    name: "Getting Started",
-                    path: "/[lang]/[version]/getting_started"
-                },
-                {
-                    name: "Why Make This?",
-                    path: "/[lang]/[version]/why"
-                },
-            ],
-        },
-    ];
+    import {tabs} from "../data/data";
 
     function getHref(path: string): string {
         return path.replace("[lang]", "en").replace("[version]", "v1");
@@ -34,11 +8,17 @@
 
 </script>
 
-<div id="toc-pane" class="flex flex-col bg-blue-900 border-r-4 border-red-800 w-[15%] overflow-scroll">
+<div id="toc-pane" class="flex flex-col bg-blue-900 border-r-4 border-red-800 w-[15%] min-w-[15%] max-w-[15%] overflow-scroll">
     {#each tabs as tab}
         <div class="flex flex-col px-2">
             <!-- Add expand/collapse symbol later -->
-            <div class="text-white text-xl">{tab.name}</div>
+            <div class="text-white text-xl flex flex-row justify-between">
+                {tab.name}
+                <button on:click={() => {
+                    console.log("test")
+                    tab.collapsed = !tab.collapsed
+                    }} class="rounded-md bg-gray-500 px-2">{tab.collapsed ? ">" : "v"}</button>
+            </div>
             {#if !tab.collapsed}
                 {#each tab.tabItems as item}
                     <a class="text-red-400 px-2" href={getHref(item.path)}>{item.name}</a>
